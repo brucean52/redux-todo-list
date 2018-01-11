c7f312ff628d76ce828a345e256540b33f90d58d
brucean52 <bruce.an52@gmail.com>
2018-01-11 12:42:20 -0800

    finish setting up redux

---
+++ src/components/add_form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddForm extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Add Todo Add Item</h1>
+            </div>
+        );
+    }
+}
+
+export default AddForm;
